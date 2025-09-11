b7a92a9da4406298eb83daf6ac9b5704b5a958c1
lu2311 <luisyraita2306@gmail.com>
2025-09-10 23:49:44 -0500

    Commit

---
+++ JavaScript2/ejercicio 3/logics.js
@@ -0,0 +1,27 @@
+alumno[{
+    nombre:"Luis Flores",
+    email:"Luis@gmail.com",
+    materia:"Fisica 3 "
+},
+alumno[{
+    nombre:"Naomi Gonzales",
+    email:"Naomi@gmail.com",
+    materia:"Herramientas"
+},
+alumno[{
+    nombre:"Kamilah FLores",
+    email:"Kamilah@gmail.com",
+    materia:"Quimica 2"
+},
+alumno[{
+    nombre:"Alex Suarez",
+    email:"Alex@gmail.com",
+    materia:"Redes "
+},
+alumno[{
+    nombre:"Carlos Mass",
+    email:"Carlos@gmail.com",
+    materia:"Mate 3 "
+},
+
+]
